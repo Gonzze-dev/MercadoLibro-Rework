089bf43c7aa45ba0a95dc8310b21686254d554a7
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-18 12:52:26 -0300

    Create repo pattern for actions in DB

---
+++ MercadoLibro/Features/UserFeature/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using MercadoLibro.Features.Filters;
+using MercadoLibroDB;
+using MercadoLibroDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercadoLibro.Features.UserFeature
+{
+    public class UserRepository(
+        MercadoLibroContext context    
+    )
+    {
+        readonly MercadoLibroContext _context = context;
+
+        [ServiceFilter(typeof(TransactionFilter))]
+        public async Task<User> AddUser(User user, UserAuth userAuth)
+        {
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            userAuth.UserID = user.Id;
+
+            _context.UserAuth.Add(userAuth);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
