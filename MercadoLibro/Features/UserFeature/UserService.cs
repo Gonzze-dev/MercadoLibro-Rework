5ff86c36fe7ed0851b79e5994d3ac1fa52574e29
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-18 12:53:04 -0300

    Create for prossesing data

---
+++ MercadoLibro/Features/UserFeature/UserService.cs
@@ -0,0 +1,27 @@
+﻿using MercadoLibroDB.Models;
+
+namespace MercadoLibro.Features.UserFeature
+{
+    public class UserService(
+        UserRepository userRepository    
+    )
+    {
+        readonly UserRepository _userRepository = userRepository;
+        public async Task<User> AddUser(
+            string name,
+            string email,
+            string password
+        )
+        {
+            //veryfing if the email exists
+            //hashing the password
+            
+            User user = new(name, email);
+            UserAuth userAuth = new(password);
+
+            var newUser = await _userRepository.AddUser(user, userAuth);
+
+            return newUser;
+        }
+    }
+}
