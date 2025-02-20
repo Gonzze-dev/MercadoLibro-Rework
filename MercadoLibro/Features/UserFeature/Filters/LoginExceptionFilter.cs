4a38b206cfb9d867b28290040af3c76bdbfa7078
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-20 18:22:46 -0300

    Add LoginExeptionFilter

---
+++ MercadoLibro/Features/UserFeature/Filters/LoginExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MercadoLibro.Features.UserFeature.Filters
+{
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class LoginExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            context.Result = exception switch
+            {
+                ArgumentNullException or BadHttpRequestException =>
+                    new BadRequestObjectResult(exception.Message),
+
+                UnauthorizedAccessException =>
+                    new ObjectResult(exception.Message) { StatusCode = 401 },
+
+                KeyNotFoundException =>
+                    new NotFoundObjectResult(exception.Message),
+
+                _ => new ObjectResult("An unexpected error occurred.") { StatusCode = 500 }
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
