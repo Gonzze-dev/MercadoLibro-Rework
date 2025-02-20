edbace4888b99b186d3735d5ee71508a33cdd39c
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-20 18:23:20 -0300

    Add Helper for password

---
+++ MercadoLibro/Features/UserFeature/UserHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace MercadoLibro.Features.UserFeature
+{
+    public static class UserHelper
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
