f15b9b521213ffb6b9e62e60e4e8b09afc8b1752
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-03 13:39:20 -0300

    refact: property requiered

--- MercadoLibro/Features/AuthFeature/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MercadoLibro.Features.AuthFeature.DTOs
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
---
+++ MercadoLibro/Features/AuthFeature/DTOs/Request/SingUpAuthRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MercadoLibro.Features.AuthFeature.DTOs.Request
+{
+    public class SingUpAuthRequest
+    {
+        public required string Name;
+        public required string Email;
+        public required string ProviderId;
+        public required string AuthMethod;
+    }
+}
