16c508e1509e58ab4e1cdae7106a22257cbc2c55
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-07 00:01:48 -0300

    refact: rename SingUpAuthRequest to SingUpSocialRequest

---
+++ MercadoLibro/Features/AuthFeature/DTOs/Request/SignUpSocialRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MercadoLibro.Features.AuthFeature.DTOs.Request
+{
+    public class SignUpSocialRequest
+    {
+        [Required]
+        public required string IdToken { get; set; }
+
+        [Required]
+        public required string AuthMethod { get; set; }
+    }
+}
