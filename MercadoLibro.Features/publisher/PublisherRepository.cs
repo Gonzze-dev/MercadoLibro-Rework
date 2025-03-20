d76aaa7a40dcca725eb295334d7c521e9bd09945
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-20 11:57:52 -0300

    freat: add publisher repository

---
+++ MercadoLibro.Features/publisher/PublisherRepository.cs
@@ -0,0 +1,35 @@
+﻿using MercadoLibroDB;
+using MercadoLibroDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercadoLibro.Features.publisher
+{
+    public class PublisherRepository(
+        TransactionDB transactionDB
+    )
+    {
+        readonly MercadoLibroContext _context = transactionDB.Context;
+
+        public async Task<IEnumerable<Publisher>> GetAll() =>
+             await _context.Publisher.ToListAsync();
+
+        public async Task<Publisher?> Get(string name) =>
+             await _context.Publisher.FirstOrDefaultAsync(p =>
+                p.Name == name
+            );
+        
+        public async Task AddAsync(Publisher publisher) =>
+            await _context.Publisher.AddAsync(publisher);
+
+        public void Update(Publisher publisher) =>
+            _context.Publisher.Update(publisher);
+
+        public void Delete(Publisher publisher) =>
+            _context.Publisher.Remove(publisher);
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
