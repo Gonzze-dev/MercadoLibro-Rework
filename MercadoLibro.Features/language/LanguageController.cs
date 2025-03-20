c9435f339eed67bbf7e986728bf8e7a62f173faf
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-20 10:41:53 -0300

    feat: add language controller

---
+++ MercadoLibro.Features/language/LanguageController.cs
@@ -0,0 +1,75 @@
+﻿using MercadoLibroDB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercadoLibro.Features.language
+{
+    [ApiController]
+    public class LanguageController(
+        LanguageService languageService    
+    ) : ControllerBase
+    {
+        readonly LanguageService _service = languageService;
+
+        [HttpGet]
+        [Route("api/language")]
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<Language> languages = await _service.GetAll();
+
+            return Ok(languages);
+        }
+
+        [HttpPost()]
+        [Route("api/language/{name}")]
+        public async Task<IActionResult> Add(
+            string name
+        )
+        {
+            var language = await _service.Add(name);
+
+            if (_service.Errors.Count > 0)
+            {
+                int statusCode = _service.Errors.First().StatusCode;
+                return StatusCode(statusCode, _service.Errors);
+            }
+
+            return Ok(language);
+        }
+
+        [HttpPut()]
+        [Route("api/language/{name}/{newName}")]
+        public async Task<IActionResult> Update(
+            string name,
+            string newName
+        )
+        {
+            var language = await _service.Update(name, newName);
+
+            if (_service.Errors.Count > 0)
+            {
+                int statusCode = _service.Errors.First().StatusCode;
+                return StatusCode(statusCode, _service.Errors);
+            }
+
+            return Ok(language);
+        }
+
+        [HttpDelete()]
+        [Route("api/language/{name}")]
+        public async Task<IActionResult> Delete(
+            string name
+        )
+        {
+            var language = await _service.Delete(name);
+
+            if (_service.Errors.Count > 0)
+            {
+                int statusCode = _service.Errors.First().StatusCode;
+                return StatusCode(statusCode, _service.Errors);
+            }
+
+            return Ok(language);
+        }
+
+    }
+}
