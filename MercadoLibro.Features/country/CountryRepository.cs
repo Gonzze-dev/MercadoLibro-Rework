100e6be3b607e2de37bd1946f0c3ea90e43a5fdb
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-21 17:49:26 -0300

    feat: add country repository

---
+++ MercadoLibro.Features/country/CountryRepository.cs
@@ -0,0 +1,34 @@
+﻿using MercadoLibroDB;
+using MercadoLibroDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercadoLibro.Features.country
+{
+    public class CountryRepository(
+        TransactionDB transactionDB    
+    )
+    {
+        readonly MercadoLibroContext _context = transactionDB.Context;
+
+        public async Task<IEnumerable<Country>> GetAll() =>
+            await _context.Country.ToListAsync();
+
+        public async Task<Country?> Get(string name) =>
+            await _context.Country.FirstOrDefaultAsync(c =>
+                c.Name == name
+            );
+
+        public async Task AddAsync(Country country) =>
+            await _context.Country.AddAsync(country);
+
+        public void Update(Country country) =>
+            _context.Country.Update(country);
+
+        public void Remove(Country country) =>
+            _context.Country.Remove(country);
+
+        public async Task SaveChangesAsync() =>
+            await _context.SaveChangesAsync();
+        
+    }
+}
