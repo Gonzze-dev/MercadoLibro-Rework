56b6b140aeeffbdeab0cb81f1d625b657cd67b9f
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-25 12:23:16 -0300

    feat: add interface base for entities repositoryes

---
+++ MercadoLibro.Features/General/interfaces/IEntityBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MercadoLibro.Features.General.interfaces
+{
+    public interface IEntityBaseRepository<TEntity, T> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity?> Get(T id);
+        Task<TEntity?> Add(T element);
+        Task<TEntity?> Update(TEntity uElement, T element);
+        Task<TEntity?> Delete(T element);
+        Task SaveChangesAsync();
+    }
+}
