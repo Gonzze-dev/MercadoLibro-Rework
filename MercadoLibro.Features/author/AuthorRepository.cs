d3d32cdd00896277f1f3239f421509db99ad52d0
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-24 21:16:01 -0300

    feat: add author repository

---
+++ MercadoLibro.Features/author/AuthorRepository.cs
@@ -0,0 +1,68 @@
+﻿using MercadoLibroDB;
+using MercadoLibroDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercadoLibro.Features.author
+{
+    public class AuthorRepository(
+        TransactionDB transactionDB    
+    )
+    {
+        readonly MercadoLibroContext _context = transactionDB.Context;
+
+        
+        public async Task<IEnumerable<Author>> GetAll() =>
+            await _context.Author.ToListAsync();
+
+        public async Task<Author?> Get(string name)
+        {
+            Author? author = await _context.Author.FirstOrDefaultAsync(author =>
+                                author.Name == name
+                             );
+            
+            return author;
+        }
+        public async Task<Author?> Add(string name)
+        {
+            Author? author = await Get(name);
+
+            if (author is not null) return null;
+
+            author = new() 
+            { 
+                Name = name
+            };
+
+            await _context.Author.AddAsync(author);
+
+            return author;
+        }
+
+        public async Task<Author?> Update(Author uAuthor, string name)
+        {
+            Author? author = await Get(name);
+
+            if (author is not null) return null;
+
+            uAuthor.Name = name;
+
+            _context.Author.Update(uAuthor);
+
+            return uAuthor;
+        }
+
+        public async Task<Author?> Delete(string name)
+        {
+            Author? author = await Get(name);
+
+            if (author is null) return null;
+
+            _context.Author.Remove(author);
+
+            return author;
+        }
+
+        public async Task SaveChangesAsync() => 
+            await _context.SaveChangesAsync();
+    }
+}
