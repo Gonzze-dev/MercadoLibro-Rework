2faad0a13471b7e7a37cf55a08a2079ef6319352
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-25 12:23:51 -0300

    feat: add DTOs for request of GenreController

---
+++ MercadoLibro.Features/genre/DTOs/GenreReq.cs
@@ -0,0 +1,7 @@
+﻿namespace MercadoLibro.Features.genre.DTOs
+{
+    public class GenreReq
+    {
+        public required string Name { get; set; }
+    }
+}
---
+++ MercadoLibro.Features/genre/DTOs/UpdateGenreReq.cs
@@ -0,0 +1,8 @@
+﻿namespace MercadoLibro.Features.genre.DTOs
+{
+    public class UpdateGenreReq
+    {
+        public required string OldName { get; set; }
+        public required string NewName { get; set; }
+    }
+}
