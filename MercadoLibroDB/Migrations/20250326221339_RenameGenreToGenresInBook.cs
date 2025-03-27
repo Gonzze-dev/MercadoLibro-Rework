9c93f1bd6ba0c5f1aea1cd5f9e7800f30edba4b7
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-26 21:20:53 -0300

    refactor: add migration for renaming property "Genre" to "Genres"

---
+++ MercadoLibroDB/Migrations/20250326221339_RenameGenreToGenresInBook.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MercadoLibroDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameGenreToGenresInBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Genre_GenreId",
+                table: "BookGenre");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreId",
+                table: "BookGenre",
+                newName: "GenresId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenre_GenreId",
+                table: "BookGenre",
+                newName: "IX_BookGenre_GenresId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Genre_GenresId",
+                table: "BookGenre",
+                column: "GenresId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Genre_GenresId",
+                table: "BookGenre");
+
+            migrationBuilder.RenameColumn(
+                name: "GenresId",
+                table: "BookGenre",
+                newName: "GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenre_GenresId",
+                table: "BookGenre",
+                newName: "IX_BookGenre_GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Genre_GenreId",
+                table: "BookGenre",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
