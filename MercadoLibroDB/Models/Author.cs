941bc659015491f0e73b5c546a72e2eea4476adf
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-03-26 21:18:49 -0300

    refactor: add JsonIgnore attribute in book collection to prevent infinite recursion

--- MercadoLibroDB/Models/Author.cs
+++ MercadoLibroDB/Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace MercadoLibroDB.Models
 {
@@ -10,6 +11,7 @@ namespace MercadoLibroDB.Models
         public int Id { get; set; }
         public required string Name { get; set; }
 
+        [JsonIgnore]
         public virtual ICollection<Book>? Books { get; set; }
     }
 }
